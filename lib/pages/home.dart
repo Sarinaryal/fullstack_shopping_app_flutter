824e13ee0faae208ee00e791a6dad092e456932d
Sarina <aryalsarinaa@gmail.com>
2025-05-05 23:33:04 +0545

    designing the categories UI

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,24 +9,156 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List categories = [
+    "images/headphone_icon.png",
+    "images/laptop.png",
+    "images/watch.png",
+    "images/TV.png",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff2f2f2),
       body: Container(
-        margin: EdgeInsets.only(top: 40, left: 20),
+        margin: EdgeInsets.only(top: 40, left: 20, right: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('hey, Sarina', style: AppWidget.boldTextFieldStyle()),
-            Text(
-              'Good Morning!!',
-              style: TextStyle(
-                color: Colors.black45,
-                fontSize: 20.0,
-                fontWeight: FontWeight.w500,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Text('Hey, Sarina', style: AppWidget.boldTextFieldStyle()),
+
+                    Text(
+                      'Good Morning!!',
+                      style: AppWidget.lightTextFieldStyle(),
+                    ),
+                  ],
+                ),
+
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(60),
+                  child: Image.asset(
+                    "images/boy.jpg",
+                    height: 70,
+                    width: 70,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 30),
+
+            //Search Product Text Field
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width:
+                  MediaQuery.of(
+                    context,
+                  ).size.width, //uses complete width pf the screen
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Search Products',
+                  hintStyle: AppWidget.lightTextFieldStyle(),
+                  prefixIcon: Icon(Icons.search, color: Colors.black),
+                ),
               ),
             ),
+
+            SizedBox(height: 20),
+
+            //categories and filters row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Categories', style: AppWidget.mediumTextStyle()),
+                Text(
+                  'see all',
+                  style: TextStyle(
+                    color: Colors.deepOrangeAccent,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            Row(
+              children: [
+                Container(
+                  height: 130,
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.only(right: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrangeAccent,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+
+                  child: Center(
+                    child: Text(
+                      'All',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 130,
+                    child: ListView.builder(
+                      itemCount: categories.length,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return CategoryTile(image: categories[index]);
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            
+          
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final String image;
+  const CategoryTile({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.only(right: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(image, height: 50, width: 50, fit: BoxFit.cover),
+
+          Icon(Icons.arrow_forward),
+        ],
       ),
     );
   }
--- lib/widget/support_widget.dart
+++ lib/widget/support_widget.dart
@@ -8,4 +8,28 @@ class AppWidget {
       fontWeight: FontWeight.bold,
     );
   }
+
+  static TextStyle lightTextFieldStyle() {
+    return TextStyle(
+      color: Colors.black45,
+      fontSize: 20.0,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle mediumTextStyle() {
+    return TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+    );
+  }
+
+  static TextStyle smallTextStyle() {
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+    );
+  }
 }
