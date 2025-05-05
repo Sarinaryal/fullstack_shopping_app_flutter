4bf0f1389e11b8f8f3dabc89565c1114194922ac
Sarina <aryalsarinaa@gmail.com>
2025-05-05 12:55:58 +0545

    Starting new project

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffecefe8),
+
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Image.asset("images/headphone.PNG")],
+          ),
+        ),
+      ),
+    );
+  }
+}
